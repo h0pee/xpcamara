57c44b47aed0837745797d30723e519fb2130bc9
Patrick <patcpv@gmail.com>
2021-02-02 17:47:21 -0300

    slide-inicial

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-var hamburguer = window.document.querySelector('.hamburguer')
+var hamburguer=window.document.querySelector(".hamburguer")
 
-hamburguer.addEventListener('click', ()=>
-    window.document.querySelector('.container').classList.toggle('show-menu') 
+hamburguer.addEventListener("click", ()=>
+    document.querySelector(".container").classList.toggle("show-menu")
 )
-    
+
+let time=4000,
+    imagemAtual=0,
+    imagens=document.querySelectorAll("#img-wrapper img")
+    maxImagens=imagens.length;
+
+function proxImagem(){
+    imagens[imagemAtual]
+    .classList.remove("selecionado")
+
+    imagemAtual++
+
+    if(imagemAtual>=maxImagens){
+        imagemAtual=0
+    }
+
+    imagens[imagemAtual]
+    .classList.add("selecionado")
+}      
+
+function start(){
+    setInterval(()=>{
+        proxImagem()
+    }, time)
+}
+
+window.addEventListener("load", start)
