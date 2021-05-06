955725bf0ca2c9221dd5ea0f9fcf44a66de3b924
Patrick <patcpv@gmail.com>
2021-05-05 23:45:22 -0300

    travada

--- gallery.js
+++ gallery.js
@@ -1,17 +1,21 @@
-let imagens=document.querySelectorAll('.small_img');
+let images=document.querySelectorAll('.small_img');
 let modal=document.querySelector('.modal');
 let modalImg=document.querySelector('#modal_img');
-let btClose=document.querySelector('#bt_close');
+const btClose = document.getElementById('bt_close')
 let srcVal="";
 
-for(let i=0; imagens.length;i++){
-    imagens[i].addEventListener('click', function(){
-        srcVal=imagens[i].getAttribute('src');
+for(let i=0; images.length; i++){
+    console.log(images[i])
+    images[i].addEventListener('click', function(){
+        srcVal=images[i].getAttribute('src');
         modalImg.setAttribute('src', srcVal);
         modal.classList.toggle('modal_active');
     });
 }
 
+
 btClose.addEventListener('click', function(){
     modal.classList.toggle('modal_active');
 });
+
+
